1397a32bb68795d061f0c8406564187e285d97b9
Jay Ravalani <jay.ravalani746@gmail.com>
2024-07-24 22:41:00 +0530

    Reder the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -14,3 +15,17 @@ with col2:
     Recent graduate of IBM's Data Engineering Specialization, eager to apply technical skills and analytical mindset to solve complex data challenges in a professional setting.
     """
     st.info(content)
+
+st.write("Below you can find some of the apps I have build in Python. Feel free to contact me!")
+
+col3, col4 = st.columns(2)
+
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, x in df[:10].iterrows():
+        st.header(x["title"])
+
+with col4:
+    for index, x in df[10:].iterrows():
+        st.header(x["title"])
