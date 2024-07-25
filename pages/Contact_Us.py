1fee8d742d57e559898002bde09e4275a972d51d
Jay Ravalani <jay.ravalani746@gmail.com>
2024-07-25 19:32:10 +0530

    send emails using form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,21 @@
 import streamlit as st
+from send_mail import send_mail
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    st.subheader("Email Address:")
+    user_email = st.text_input(" ", placeholder="abc@google.com")
+    st.subheader("Message")
+    raw_message = st.text_area(" ", placeholder="Hello World")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+    """
+    submit_button = st.form_submit_button("Submit")
+    if submit_button:
+        print("Submit button was pressed!")
+        send_mail(message)
+        st.info("Your email was send successfully!")
---
+++ send_mail.py
@@ -0,0 +1,14 @@
+import smtplib, ssl
+
+
+def send_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jay.ravalani746@gmail.com"
+    password = "xhwjnxrvmdodahcb"
+    receiver = "jay.ravalani746@gmail.com"
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
